dab601b2fb5e3a08fd0dd0c2062a87902662f54b
SergeyTasun <iseryog@gmail.com>
2017-09-13 17:28:45 +0300

    add throw new Error

--- Занятие №9/5/js.js
+++ Занятие №9/5/js.js
@@ -12,21 +12,36 @@ arr и два числа (first и second) – порядковые номера
 
 var array = [1, 8, 5, 9, 3, 7, 4];
 
-
 function sumSliceArray(array, first, second) {
    try { 
-      if (typeof(array[first]) !== "number" 
-      ||  typeof(array[second]) !== "number" 
-      ||  first < 0 
-      ||  second < 0 
-      ||  first > array.length 
-      ||  second > array.length){  
-         array.err();  
+      if (typeof(array[first]) !== "number") {
+         throw new Error("Первый элемент не является числом");
       }
-      return array[first] + array[second]; 
+           
+      if (typeof(array[second]) !== "number") {
+         throw new Error("Ощибка!!! Второй элемент не является числом");
+      }
+     
+      if (first < 0) {
+            throw new Error("Ощибка!!! Первый элемент меньше нуля");
+      }
+      
+      if (second < 0) {
+            throw new Error("Ощибка!!!Второй элемент меньше нуля");
+      }
+      
+      if (first > array.length) {
+            throw new Error("Ощибка!!! Первый элемент больше длинны массива");
+      }
+      
+      if (second > array.length) {
+            throw new Error("Ощибка!!! Второй элемент больше длинны массива");
+      }
+      
+   return array[first] + array[second]; 
    } catch (error) {
-      return "Обработка исключения";
-   }   
+      return error.message
+   }
 }
 
-console.log(sumSliceArray(array, 2, 5));
+console.log(sumSliceArray(array, 2, 50));
