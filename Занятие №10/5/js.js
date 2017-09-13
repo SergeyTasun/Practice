7b8361ce302d31703d1e40d82fb9a5b36c986343
SergeyTasun <iseryog@gmail.com>
2017-09-13 17:08:10 +0300

    replace with concat

--- Занятие №10/5/js.js
+++ Занятие №10/5/js.js
@@ -14,17 +14,11 @@ var arr = matrixToArray(arr);
 console.log(arr); // [1, 2, 3, 4, 5, 6, 7, 8, 9]*/
 
 function matrixToArray(arr) {
-   arr = arr.reduce(reducer);
-   arr = arr.split(',');
-   return arr.map(toNumber);
-}
-
-function reducer(accumulator, currentValue) {
-   return accumulator + "," + currentValue;
-}
-
-function toNumber(value) {
-   return Number(value);
+   return arr.reduce(
+      function (acc, cValue) {
+         return acc.concat(cValue)   
+      }
+   );
 }
 
 var arr = [[1, 2], [3, 4, 5], [6, 7, 8], [9]];
